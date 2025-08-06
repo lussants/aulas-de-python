fdb5df9e3dabb1613057b9833a245b341b01add4
Emanuelle <331manuh@gmail.com>
2025-08-06 16:59:44 -0300

    INTERVALOS

--- Final Fantasy/Main.py
+++ Final Fantasy/Main.py
@@ -1,9 +1,53 @@
 def main() :
-    num= int (input("Digite um número: "))
-    divisores = ""
-    i = 10
-    j = 40
+    inicio = int (input ("inicio : "))
+    fim = int (input ("fim : "))
 
-    while i < j
+    primos = ""
+
+
+    if inicio < fim:
+        while inicio <= fim:
+            i = 2
+
+            while inicio > i: 
+                if inicio % i == 0:
+                    break
+                i += 1 
+
+            if inicio == i and inicio > 1:
+                primos += "\n" + str(inicio)
+
+            inicio += 1
+    elif inicio > fim:
+        while inicio >= fim:
+
+            i = 2 
+
+            while inicio != i and inicio > 1:
+                if inicio % i == 0:
+                    break
+                i += 1
+
+            if inicio == i and inicio > 1:
+                primos += "\n" + str (inicio)
+            inicio -= 1
+    else:
+        i = 2
+
+        while inicio != i and inicio > 1:
+            if inicio % i == 0:
+                    break
+            i += 1
+
+        if inicio == i and inicio > 1:
+            primos += "\n" + str (inicio)
+    if primos == "":
+        primos += " intervalo sem primos"
+
+    print("Primos: ", primos)
+
+
+
+
     return 0
 main()
